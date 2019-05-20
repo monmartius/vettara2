810ed20ef13e801624169db0f5e298d86694d768
monmartius <monmarius@gmail.com>
2019-05-20 21:37:47 +0700

    -sm first slider cards

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -15,108 +15,64 @@ console.log(wow);
 
 let $popularServicesSlider = $('#popular-services-slider');
 
-$popularServicesSlider.slick({
 
-		// dots: true,
-		infinite: true,
-		speed: 4000,
-		autoplay: true,
-		autoplaySpeed: 4000,
-		// variableWidth: false,
-		adaptiveHeight: false
-		// ,
-		// vertical: true
+$popularServicesSlider.animationEffect = 'bounceIn';
 
-});
+$popularServicesSlider.init = function (){
 
+	$popularServicesSlider.slick({
 
-let animationEffect = 'bounceIn';
-let $popularServicesSliderSlides = $('.popular-services-slider-slides').detach();
-// let $popularServicesSliderSlides = $('.popular-services-slider-slides');
-// console.log('$popularServicesSliderSlides');
-// console.log($popularServicesSliderSlides);
+			// dots: true,
+			infinite: true,
+			speed: 1000,
+			autoplay: true,
+			autoplaySpeed: 4000,
+			// variableWidth: false,
+			adaptiveHeight: false
+			// ,
+			// vertical: true
 
-let $slides = $('.popular-services-slider-slide', $popularServicesSliderSlides);
+	});
 
-// console.log($slides)
+	let $slides = this.slick('getSlick').$slides;
+	
+	for(let i = 0 ; i < $slides.length ; i++){
 
-// $popularServicesSlider.slick({autoplay: true});
-
-for(let i = 0 ; i < $slides.length ; i++){
-
-	$($slides[i]).attr('data-user-slide-index', i);
-
-	let $animatedElements = $('.element-animated', $slides[i]);
+		let $animatedElements = $('.element-animated', $slides[i]);
 
 
-	$animatedElements.addClass('duration3s');
-	$animatedElements.addClass('animation-paused');			
-	$animatedElements.addClass(animationEffect);	
+		$animatedElements.addClass('animation-duration');
+		$animatedElements.addClass('animation-paused');			
+		$animatedElements.addClass('animation-delay');			
+		$animatedElements.addClass(this.animationEffect);	
 
-	$popularServicesSlider.slick('slickAdd', $slides[i]);
+		$popularServicesSlider.slick('slickAdd', $slides[i]);
+	}
+
 }
 
-/*
 
-slickCurrentSlide
-	none
-	Returns the current slide index
-
-
-slickGoTo
-	int : slide number, boolean: dont animate
-	Navigates to a slide by index
-slickNext
-	none
-	Navigates to the next slide
-slickPrev
-	none
-	Navigates to the previous slide
-
-
-slickAdd
- 	html or DOM object, index, addBefore
-	Add a slide. If an index is provided, will add at that index, or before if addBefore is set. If no index is provided, add to the end or to the beginning if addBefore is set. Accepts HTML String || Object
-slickRemove
-
-*/
-
-
+// $popularServicesSlider.init();
 
 $popularServicesSlider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
 
-
-	// console.log($(slick.$slides[currentSlide]));
-
-// alert('currentSlide ' + currentSlide) 
-
-
 	let $slides = $('#popular-services-slider .slick-slide');
-
-	// let $animatedElementsClonedSlides = $('.element-animated', $clonedSlides);
-
-	// let $currentSlide = $(slick.$slides[currentSlide]);
-	// let $nextSlide = $(slick.$slides[nextSlide]);
 
 	for(let i = 0 ; i < $slides.length; i++){
 
 
 		let $animatedElements = $('.element-animated', $slides[i]);
 		let slideIndex = $($slides[i]).hasClass('slick-current');
-	// console.log($animatedElements.html());
-	// console.log($animatedElements )
 
 		if(!$($slides[i]).hasClass('slick-current')){
 
 			$animatedElements.removeClass('animation-running')
-				.removeClass('s-current')
+				// .removeClass('s-current')
 				.addClass('animation-paused')		
+				.addClass('animation-delay')		
 				.each(restartAnimation);
-// alert('i ' + i)
-
 		}
 	}
-// alert(currentSlide)
 });
 
 
@@ -124,6 +80,7 @@ function restartAnimation(index, element){
  	var newElement = element.cloneNode(true);
     element.parentNode.replaceChild(newElement, element);
 }
+
 
 $popularServicesSlider.on('afterChange', function(event, slick, currentSlide, nextSlide){
 
